5b8b77f61dec49067febe2ba4d4c999d1bb97b4e
YuriBaidhy <baidhy@uol.com.br>
2025-03-18 19:23:53 +0000

    add comparação entre as cartas com if else

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -44,7 +44,7 @@ int main() {
     printf("Digite o nome da Cidade: ");
     scanf("%s", &Cidade1);
         
-    printf("Digite a sua População: ");
+    pintf("Digite a sua População: ");
     scanf("%uld", &Populacao1); 
     
     printf("Informe a sua Área em km²: ");
@@ -57,10 +57,13 @@ int main() {
     scanf("%d", &Pontos1);
     printf("\n");
 
+    //Para calcular a Densidade Populacional deve-se dividir a População pela Área da cidade
+    //Para calcular o PIB per Capita deve-se dividir o PIB pela População da cidade
+    //Em ambos os cálculos, atenta-se para realizar a conversão explicita (Casting) da Variável "Populacao1" para float
+
     Densidade1 = (float) Populacao1 / Area1;
     Pibpercapita1 = (PIB1) / (float) Populacao1;
-
-   
+ 
     //Perguntas para a Carta 02
     printf("Carta 02\n");
     printf("Digite uma letra de 'A' a 'H' representando um dos oito estados: ");
@@ -88,7 +91,6 @@ int main() {
     Densidade2 = (float) Populacao2 / Area2;
     Pibpercapita2 = (PIB2) / (float) Populacao2;
 
-
     //Após respondido o questionário, o sistema exibirá os dados de ambas as cartas na sequência.
 
     printf("Carta 1\n");
@@ -102,9 +104,12 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km²\n", Densidade1);
     printf("PIB per Capita: R$ %.2f\n", Pibpercapita1);
 
+    //Para calcular o valor do "SUPER PODER" da "CARTA 01", deve-se somar os valores das variáveis...
+    //... "Populacao1", "Area1", "PIB1", "Pontos1", "Pibpercapita1" e o inverso (*-1) da "Densidade1" 
+
     superpoder1 = (float)Populacao1 + Area1 + PIB1 + Pontos1 + Pibpercapita1 + (Densidade1 * -1);
 
-    printf("SUPER PODER CARTA 01: %f\n", superpoder1);
+    printf("SUPER PODER CARTA 01: %.f\n", superpoder1);
     printf("\n");
 
     printf("Carta 2\n");
@@ -118,20 +123,60 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km²\n", Densidade2);
     printf("PIB per Capita: R$ %.2f\n", Pibpercapita2);
 
+    //Para calcular o valor do "SUPER PODER" da "CARTA 02", deve-se somar os valores das variáveis...
+    //... "Populacao2", "Area2", "PIB2", "Pontos2", "Pibpercapita2" e o inverso (*-1) da "Densidade2"   
+
     superpoder2 = (float)Populacao2 + Area2 + PIB2 + Pontos2 + Pibpercapita2 + (Densidade2 * -1);
 
-    printf("SUPER PODER CARTA 02: %f\n", superpoder2);
+    printf("SUPER PODER CARTA 02: %.f\n", superpoder2);
     printf("\n");
 
+
+    //Para fazer a comparação entre as cartas de cada atributo, utilizar o operador relacional ">"
+    //Se os valores das variáveis da "CARTA 01", forem maiores que os valores da "CARTA 02", o programa...
+    //... retornará com a resposta "1" que significa "verdadeiro", caso contrário a resposta será "0" que...
+    //... significa "falso", com exeção do atributo "Densidade Populacional" que deverá ser utilizado...
+    //... o operador relacional "<", uma vez que, quanto menor for a "Densidade Populacional" melhor será...
+    ///... o resultado do atributo.  
+
     printf("***COMPARAÇÃO ENTRE AS CARTAS***\n");
-    printf("População: Carta 1 Venceu (%u)\n", Populacao1 > Populacao2);
-    printf("Área: Carta 1 Venceu (%d)\n", Area1 > Area2);
-    printf("PIB: Carta 1 Venceu (%d)\n", PIB1 > PIB2);
-    printf("Número de Pontos Turísticos: Carta 1 Venceu (%d)\n", Pontos1 > Pontos2);
-    printf("Densidade Populacional: Carta 1 Venceu (%d)\n", Densidade1 < Densidade2);
-    printf("PIB per Capita: Carta 1 Venceu (%d)\n", Pibpercapita1 > Pibpercapita2);
-    printf("Super Poder: Carta 1 Venceu (%d) \n", superpoder1 > superpoder2);
+    printf("População: Carta 1 Venceu: %u\n", Populacao1 > Populacao2);
+    printf("Área: Carta 1 Venceu: %d\n", Area1 > Area2);
+    printf("PIB: Carta 1 Venceu: %d\n", PIB1 > PIB2);
+    printf("Número de Pontos Turísticos: Carta 1 Venceu: %d\n", Pontos1 > Pontos2);
+    printf("Densidade Populacional: Carta 1 Venceu: %d\n", Densidade1 < Densidade2);
+    printf("PIB per Capita: Carta 1 Venceu: %d\n", Pibpercapita1 > Pibpercapita2);
+    printf("Super Poder: Carta 1 Venceu: %d \n", superpoder1 > superpoder2);
     printf("\n");
+
+    // Para apresentar a comparação entre de um atrituto específico inforomando qual cidade venceu...
+    //... utilizar a estrutura de decisão composta "if-else"
+
+    //utilizando a estrutura de decisão composta "if-else" para apresentar a carta vencedeora no...
+    //... atributo "Densidade Populacional"
+    printf("Comparação entre as cartas (Atributo: Densidade Populacional)\n");
+    printf("Carta 01 - %s: %.2f hab/km²\n", Cidade1, Densidade1);
+    printf("Carta 02 - %s: %.2f hab/km²\n", Cidade2, Densidade2);
+
+        if (Densidade1 < Densidade2){
+        printf("Carta 01 - RECIFE venceu\n"), Densidade1;}
+        else {printf("Carta 02 - CARUARU venceu\n"), Densidade2;}
+        printf("\n");
+
+    //utilizando a estrutura de decisão composta "if-else" para apresentar a carta vencedeora no...
+    //... atributo "PIB per Capita"
+    printf("Comparação entre as cartas (Atributo: PIB per Capita)\n");
+    printf("Carta 01 - %s: R$ %.2f\n", Cidade1, PIB1);
+    printf("Carta 02 - %s: R$ %.2f\n", Cidade2, PIB2);
+
+        if (PIB1 > PIB2){
+        printf("Carta 01 - RECIFE venceu\n"), PIB1;}
+        else {printf("Carta 02 - CARUARU venceu\n"), PIB2;}
+        printf("\n");
+
+
+
+    
 
     
 
